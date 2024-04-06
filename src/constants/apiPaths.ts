94dfea3fe2a211fffa3f43ba1190ead729167b15
Eugene <evgenij.sleepy@gmail.com>
2024-04-06 20:29:51 +0300

    Task 4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,9 +2,10 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://va75ucwcrg.execute-api.eu-west-1.amazonaws.com/dev/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  products: "https://hs1bjvbd17.execute-api.eu-west-1.amazonaws.com",
+  products:
+    "https://va75ucwcrg.execute-api.eu-west-1.amazonaws.com/dev/products",
 };
 
 export default API_PATHS;
